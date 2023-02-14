4a78c5954592b161f90a10ff453e31caacbc7818
turboGraf <abbzew@gmail.com>
2023-02-14 12:16:58 +0200

    gallery js switcher complete, hover effects for links completed

--- app/js/main.js
+++ app/js/main.js
@@ -4,24 +4,37 @@ let themeButtonLight = document.querySelector('.button__light-theme');
 let themeButtonDark = document.querySelector('.button__dark-theme');
 
 themeButtonDark.onclick = function () {
-	document.body.classList.add('dark-theme')
-	themeButtonDark.classList.add('active-button')
-	themeButtonLight.classList.remove('active-button')
-}
+	document.body.classList.add('dark-theme');
+	themeButtonDark.classList.add('active-button');
+	themeButtonLight.classList.remove('active-button');
+};
 
 themeButtonLight.onclick = function () {
-	document.body.classList.remove('dark-theme')
-	themeButtonDark.classList.remove('active-button')
-	themeButtonLight.classList.add('active-button')
-}
+	document.body.classList.remove('dark-theme');
+	themeButtonDark.classList.remove('active-button');
+	themeButtonLight.classList.add('active-button');
+};
 
 // Side-menu switcher
-let hamburger = document.querySelector('.button__hamburger')
-let mobileNav = document.querySelector('.mobile-nav')
+let hamburger = document.querySelector('.button__hamburger');
+let mobileNav = document.querySelector('.mobile-nav');
 
 hamburger.onclick = function () {
-	mobileNav.classList.toggle('hidden')
-}
+	mobileNav.classList.toggle('hidden');
+	hamburger.classList.toggle('active-button');
+};
 
 // Gallery
+let photoPreviews = document.querySelectorAll('.preview-item');
+let activePhoto = document.querySelector('.gallery__active-photo');
 
+for (let photoPreview of photoPreviews) {
+	
+	photoPreview.onclick = function () {
+		activePhoto.src = photoPreview.querySelector('img').src;
+
+		let activePreview = document.querySelector('.active-preview');
+		activePreview.classList.remove('active-preview');
+		photoPreview.classList.add('active-preview');
+	}
+};
