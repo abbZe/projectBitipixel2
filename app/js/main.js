1aa33d7f07ce74f20fb571fdab6241669689987d
turboGraf <abbzew@gmail.com>
2023-02-20 10:06:56 +0200

    Switch tabs to spaces, bcs its better-looking choise for github and other users

--- app/js/main.js
+++ app/js/main.js
@@ -4,15 +4,15 @@ let themeButtonLight = document.querySelector('.button__light-theme');
 let themeButtonDark = document.querySelector('.button__dark-theme');
 
 themeButtonDark.onclick = function () {
-	document.body.classList.add('dark-theme');
-	themeButtonDark.classList.add('active-button');
-	themeButtonLight.classList.remove('active-button');
+  document.body.classList.add('dark-theme');
+  themeButtonDark.classList.add('active-button');
+  themeButtonLight.classList.remove('active-button');
 };
 
 themeButtonLight.onclick = function () {
-	document.body.classList.remove('dark-theme');
-	themeButtonDark.classList.remove('active-button');
-	themeButtonLight.classList.add('active-button');
+  document.body.classList.remove('dark-theme');
+  themeButtonDark.classList.remove('active-button');
+  themeButtonLight.classList.add('active-button');
 };
 
 // Side-menu switcher
@@ -20,8 +20,8 @@ let hamburger = document.querySelector('.button__hamburger');
 let mobileNav = document.querySelector('.mobile-nav');
 
 hamburger.onclick = function () {
-	mobileNav.classList.toggle('hidden');
-	hamburger.classList.toggle('active-button');
+  mobileNav.classList.toggle('hidden');
+  hamburger.classList.toggle('active-button');
 };
 
 // Gallery
@@ -29,12 +29,12 @@ let photoPreviews = document.querySelectorAll('.preview-item');
 let activePhoto = document.querySelector('.gallery__active-photo');
 
 for (let photoPreview of photoPreviews) {
-	
-	photoPreview.onclick = function () {
-		activePhoto.src = photoPreview.querySelector('img').src;
+  
+  photoPreview.onclick = function () {
+    activePhoto.src = photoPreview.querySelector('img').src;
 
-		let activePreview = document.querySelector('.active-preview');
-		activePreview.classList.remove('active-preview');
-		photoPreview.classList.add('active-preview');
-	}
+    let activePreview = document.querySelector('.active-preview');
+    activePreview.classList.remove('active-preview');
+    photoPreview.classList.add('active-preview');
+  }
 };
